262439bfef4da7d7b57ce8cb440e0a6104c721cf
GuiEstevamCorrea <guilhermecorrea.2002@hotmail.com>
2023-04-04 10:47:37 -0300

    Create of packege Chess, classes inside Chess and class UI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import boardGame.Board;
-import boardGame.Position;
 import chess.ChessMatch;
-import com.sun.security.jgss.GSSUtil;
 
 public class Main {
     public static void main(String[] args) {
