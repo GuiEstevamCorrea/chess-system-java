0c949d8220db4190e933eb1c22ec5a59a98b0e2e
GuiEstevamCorrea <guilhermecorrea.2002@hotmail.com>
2023-04-05 17:11:00 -0300

    Finalize the creation of ChessGame

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -4,36 +4,35 @@ import boardGame.Position;
 
 public class ChessPosition {
 
-    private char collumn;
+    private char column;
     private int row;
 
-    public ChessPosition(char collumn, int row) {
-        if (collumn < 'a' || collumn > 'h' || row < 1 || row > 8){
-            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
         }
-        this.collumn = collumn;
+        this.column = column;
         this.row = row;
     }
 
-    public char getCollumn() {
-        return collumn;
+    public char getColumn() {
+        return column;
     }
-
 
     public int getRow() {
         return row;
     }
 
-    protected Position toPosition (){
-        return new Position(8 - row, collumn - 'a');
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
     }
 
-    protected static ChessPosition fromPosition(Position position){
-        return new ChessPosition((char)('a' - position.getCollumn()), 8 - position.getRow());
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
-
-    public String toSTring(){
-        return " " + collumn + row;
+    @Override
+    public String toString() {
+        return "" + column + row;
     }
 }
